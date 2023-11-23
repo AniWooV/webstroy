57f39c5f2ab23e03c3cfcab5b76864e2812a9711
AniWooV <8aniwoov8@gmail.com>
2023-11-23 10:36:51 +0500

    Keep setting rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,63 +1,163 @@
 /** @type {import('next').NextConfig} */
 
+const routesBase = require("./settings/routes-base.json")
 const routesParams = require("./settings/routes-params.json")
 
 function generateRewrites() {
-	const definedRoutes = []
+	const definedRewrites = {}
 
-	routesParams.forEach((params) => {
-		const cities = `(${params.cities.join("|")})`
-		const langs = `(${params.langs.join("|")})`
+	for (let route in routesBase) {
+		const rewrites = []
 
-		const folder = [
-			{
-				type: "host",
-				value: `(${params.subdomain}\..*)`,
-			},
-		]
+		routesParams.forEach((params) => {
+			const cities = `(${params.cities.join("|")})`
+			const langs = `(${params.langs.join("|")})`
 
-		definedRoutes.push({
-			source: `/:lang${langs}/:slugs(.*)`,
-			has: folder,
-			destination: `/:slugs?lang=:lang`,
-		})
-		definedRoutes.push({
-			source: `/:slugs(.*)/:city${cities}`,
-			has: folder,
-			destination: `/:slugs?city=:city`,
-		})
-		definedRoutes.push({
-			source: `/:lang${langs}/:slugs(.*)/:city${cities}`,
-			has: folder,
-			destination: `/:slugs?lang=:lang&city=:city`,
-		})
-		definedRoutes.push({
-			source: `/:lang${langs}`,
-			has: folder,
-			destination: `/`,
-		})
-		definedRoutes.push({
-			source: `/:city${cities}`,
-			has: folder,
-			destination: `/`,
-		})
-		definedRoutes.push({
-			source: `/:lang${langs}/:city${cities}`,
-			has: folder,
-			destination: `/`,
-		})
-	})
+			const has = [{type: "host", value: `(${params.subdomain}\..*)`}]
 
-	return definedRoutes
+			rewrites.push(
+				{
+					source: `/:lang${langs}${routesBase[route]}`,
+					has,
+				},
+				{
+					source: `${routesBase[route]}/:city${cities}`,
+					has
+				},
+				{
+					source: `/:lang${langs}${routesBase[route]}/:city${cities}`,
+					has
+				},
+			)
+		})
+
+		definedRewrites[route] = rewrites
+	}
+
+	return definedRewrites
 }
 
-const routes = generateRewrites()
+// const rewrites = generateRewrites()
+// console.log(rewrites);
 
 const nextConfig = {
 	async rewrites() {
-		return routes
+		// return [
+		// 	routes.home.map((rewrite) => {
+		// 		rewrite.destination = "/"
+
+		// 		return rewrite
+		// 	}),
+		// 	routes.about.map((rewrite) => {
+		// 		rewrite.destination = "/about/"
+
+		// 		return rewrite
+		// 	}),
+		// 	routes.services.map((rewrite) => {
+		// 		rewrite.destination = "/services/"
+
+		// 		return rewrite
+		// 	}),
+		// 	routes.service.map((rewrite) => {
+		// 		rewrite.destination = "/services/:slug/?lang=:lang&city=:city"
+		// 		console.log(rewrite);
+		// 		return rewrite
+		// 	}),
+		// 	routes.portfolio.map((rewrite) => {
+		// 		rewrite.destination = "/portfolio/"
+
+		// 		return rewrite
+		// 	}),
+		// 	routes.project.map((rewrite) => {
+		// 		rewrite.destination = "/portfolio/:slug/?lang=:lang&city=:city"
+
+		// 		return rewrite
+		// 	}),
+		// 	routes.contacts.map((rewrite) => {
+		// 		rewrite.destination = "/contacts/?lang=:lang&city=:city"
+
+		// 		return rewrite
+		// 	})
+		// ].flat(Infinity)
+
+		// =========================================================================
+		
+		return[
+			{
+				source: "/:lang(ru|en)/services",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			},
+			{
+				source: "/services/:city(perm|irbit)",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			},
+			{
+				source: "/:lang(ru|en)/services/:city(perm|irbit)",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			},
+			{
+				source: "/:lang(ru|en)/services/:slug(.*-.*)",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/:slug/?lang=:lang&city=:city",
+			},
+			{
+				source: "/services/:slug(.*-.*)/:city(perm|irbit)",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/:slug/?lang=:lang&city=:city",
+			},
+			{
+				source: "/:lang(ru|en)/services/:slug(.*-.*)/:city(perm|irbit)",
+				has: [{
+					type: "host",
+					value: "(ekb\..*)"
+				}],
+				destination: "/services/:slug/?lang=:lang&city=:city",
+			},
+			{
+				source: "/:lang(ru|en)/services",
+				has: [{
+					type: "host",
+					value: "(zab\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			},
+			{
+				source: "/services/:city(chita|kr-kam)",
+				has: [{
+					type: "host",
+					value: "(zab\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			},
+			{
+				source: "/:lang(ru|en)/services/:city(chita|kr-kam)",
+				has: [{
+					type: "host",
+					value: "(zab\..*)"
+				}],
+				destination: "/services/?lang=:lang&city=:city",
+			}
+			
+		] 
 	},
-	trailingSlash: true,
 }
 
 module.exports = nextConfig
