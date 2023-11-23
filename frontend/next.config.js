3c9af0a864724bdf0ae4d92aa110550aef6168d1
AniWooV <8aniwoov8@gmail.com>
2023-11-23 10:49:52 +0500

    Added host and subdomain settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,7 +13,7 @@ function generateRewrites() {
 			const cities = `(${params.cities.join("|")})`
 			const langs = `(${params.langs.join("|")})`
 
-			const has = [{type: "host", value: `(${params.subdomain}\..*)`}]
+			const has = [{ type: "host", value: `(${params.subdomain}\..*)` }]
 
 			rewrites.push(
 				{
@@ -22,12 +22,12 @@ function generateRewrites() {
 				},
 				{
 					source: `${routesBase[route]}/:city${cities}`,
-					has
+					has,
 				},
 				{
 					source: `/:lang${langs}${routesBase[route]}/:city${cities}`,
-					has
-				},
+					has,
+				}
 			)
 		})
 
@@ -37,126 +37,47 @@ function generateRewrites() {
 	return definedRewrites
 }
 
-// const rewrites = generateRewrites()
-// console.log(rewrites);
+const rewrites = generateRewrites()
 
 const nextConfig = {
 	async rewrites() {
-		// return [
-		// 	routes.home.map((rewrite) => {
-		// 		rewrite.destination = "/"
+		return [
+			rewrites.home.map((rewrite) => {
+				rewrite.destination = "/"
 
-		// 		return rewrite
-		// 	}),
-		// 	routes.about.map((rewrite) => {
-		// 		rewrite.destination = "/about/"
+				return rewrite
+			}),
+			rewrites.about.map((rewrite) => {
+				rewrite.destination = "/about/"
 
-		// 		return rewrite
-		// 	}),
-		// 	routes.services.map((rewrite) => {
-		// 		rewrite.destination = "/services/"
+				return rewrite
+			}),
+			rewrites.services.map((rewrite) => {
+				rewrite.destination = "/services/"
 
-		// 		return rewrite
-		// 	}),
-		// 	routes.service.map((rewrite) => {
-		// 		rewrite.destination = "/services/:slug/?lang=:lang&city=:city"
-		// 		console.log(rewrite);
-		// 		return rewrite
-		// 	}),
-		// 	routes.portfolio.map((rewrite) => {
-		// 		rewrite.destination = "/portfolio/"
+				return rewrite
+			}),
+			rewrites.service.map((rewrite) => {
+				rewrite.destination = "/services/:slug/?lang=:lang&city=:city"
 
-		// 		return rewrite
-		// 	}),
-		// 	routes.project.map((rewrite) => {
-		// 		rewrite.destination = "/portfolio/:slug/?lang=:lang&city=:city"
+				return rewrite
+			}),
+			rewrites.portfolio.map((rewrite) => {
+				rewrite.destination = "/portfolio/"
 
-		// 		return rewrite
-		// 	}),
-		// 	routes.contacts.map((rewrite) => {
-		// 		rewrite.destination = "/contacts/?lang=:lang&city=:city"
+				return rewrite
+			}),
+			rewrites.project.map((rewrite) => {
+				rewrite.destination = "/portfolio/:slug/?lang=:lang&city=:city"
 
-		// 		return rewrite
-		// 	})
-		// ].flat(Infinity)
+				return rewrite
+			}),
+			rewrites.contacts.map((rewrite) => {
+				rewrite.destination = "/contacts/?lang=:lang&city=:city"
 
-		// =========================================================================
-		
-		return[
-			{
-				source: "/:lang(ru|en)/services",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			},
-			{
-				source: "/services/:city(perm|irbit)",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			},
-			{
-				source: "/:lang(ru|en)/services/:city(perm|irbit)",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			},
-			{
-				source: "/:lang(ru|en)/services/:slug(.*-.*)",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/:slug/?lang=:lang&city=:city",
-			},
-			{
-				source: "/services/:slug(.*-.*)/:city(perm|irbit)",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/:slug/?lang=:lang&city=:city",
-			},
-			{
-				source: "/:lang(ru|en)/services/:slug(.*-.*)/:city(perm|irbit)",
-				has: [{
-					type: "host",
-					value: "(ekb\..*)"
-				}],
-				destination: "/services/:slug/?lang=:lang&city=:city",
-			},
-			{
-				source: "/:lang(ru|en)/services",
-				has: [{
-					type: "host",
-					value: "(zab\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			},
-			{
-				source: "/services/:city(chita|kr-kam)",
-				has: [{
-					type: "host",
-					value: "(zab\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			},
-			{
-				source: "/:lang(ru|en)/services/:city(chita|kr-kam)",
-				has: [{
-					type: "host",
-					value: "(zab\..*)"
-				}],
-				destination: "/services/?lang=:lang&city=:city",
-			}
-			
-		] 
+				return rewrite
+			}),
+		].flat(Infinity)
 	},
 }
 
